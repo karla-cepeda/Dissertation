ed6799ab01d7128179188eb68cdd38801963f67c
karla-cepeda <d00242569@student.dkit.ie>
2021-09-06 20:15:49 +0100

    Structure of local database called Twitter

---
+++ SQL/local/twitter.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: twitter
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hashtag`
+--
+
+DROP TABLE IF EXISTS `hashtag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hashtag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(280) NOT NULL,
+  `cleaned` varchar(560) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25052 DEFAULT CHARSET=utf8 COMMENT='Catalog the hashtags.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `log_file`
+--
+
+DROP TABLE IF EXISTS `log_file`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(100) NOT NULL,
+  `filename` varchar(50) NOT NULL,
+  `extention` varchar(10) NOT NULL DEFAULT '.json',
+  `total_tweets` int(11) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `preprocessed` tinyint(4) NOT NULL DEFAULT '0',
+  `preprocessed_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10788 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `migration`
+--
+
+DROP TABLE IF EXISTS `migration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migration` (
+  `tweet_id` varchar(50) NOT NULL,
+  `migrated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`tweet_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `place`
+--
+
+DROP TABLE IF EXISTS `place`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `place` (
+  `place_id` varchar(128) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `country` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`place_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `referenced_tweet`
+--
+
+DROP TABLE IF EXISTS `referenced_tweet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `referenced_tweet` (
+  `tweet_id` varchar(128) NOT NULL,
+  `referenced_id` varchar(128) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`tweet_id`,`referenced_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweet`
+--
+
+DROP TABLE IF EXISTS `tweet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweet` (
+  `tweet_id` varchar(50) NOT NULL,
+  `original_text` varchar(6000) NOT NULL,
+  `cleaned_text` varchar(560) NOT NULL,
+  `normalized_text` varchar(560) NOT NULL,
+  `author_id` varchar(50) DEFAULT NULL,
+  `conversation_id` varchar(50) NOT NULL,
+  `in_reply_to_user_id` varchar(50) DEFAULT NULL,
+  `lang` varchar(5) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `place_id` varchar(50) DEFAULT NULL,
+  `batch_name` varchar(100) NOT NULL,
+  `key_name` varchar(10) NOT NULL,
+  `keywords` varchar(300) DEFAULT NULL,
+  `keywords_pharma` varchar(300) DEFAULT NULL,
+  `label_id` int(11) DEFAULT NULL,
+  `label` varchar(10) DEFAULT NULL,
+  `active` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`tweet_id`),
+  UNIQUE KEY `tweet_id_UNIQUE` (`tweet_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Tweets collected from Twitter API.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tweet_hashtag`
+--
+
+DROP TABLE IF EXISTS `tweet_hashtag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tweet_hashtag` (
+  `tweet_id` varchar(128) NOT NULL,
+  `hashtag_id` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Hashtag in tweet.';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `author_id` varchar(128) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `verifed` tinyint(4) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `on_query` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`author_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-06 20:14:41
