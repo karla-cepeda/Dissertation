8ce405760a2c9ce9573809c09b262e8570664080
karla-cepeda <d00242569@student.dkit.ie>
2021-09-10 15:10:57 +0100

    Remove functions to local db and included to remote.

--- Code/layer_data_access/date_data.py
+++ Code/layer_data_access/date_data.py
@@ -24,28 +24,34 @@ class data(object):
 
     def __del__(self):
         del self._db
-        
-class date_data(data):
+    
+class date_data_remote(data):
     def __init__(self):
-        super().__init__('dcovid', True)
+        super().__init__('tanniest_sentimentanalysis', False)
 
         # Get name of stored procedure
         myy = my_yaml.my_yaml_mydb()
-        sp_list = myy.get_stored_procedure_names_dcovid()
-        date_data.__sp_truncate_tables = sp_list["truncate_table"]
-        date_data.__sp_insert_date = sp_list["insert_date"]
-        date_data.__sp_insert_reference = sp_list["insert_reference"]
-        date_data.__sp_insert_date_reference = sp_list["insert_date_reference"]
-        date_data.__sp_get_dates_for_migration = sp_list["get_dates_for_migration"]
-        date_data.__sp_get_date_reference_for_migration = sp_list["get_date_reference_for_migration"]
-        date_data.__sp_get_reference_for_migration = sp_list["get_reference_for_migration"]
-        date_data.__sp_insert_migration = sp_list["insert_migration"]
-        date_data.__sp_lookup_date = sp_list["lookup_date"]
+        sp_list = myy.get_stored_procedure_names_sentimentanalysis()
+        date_data_remote.__sp_lookup_date = sp_list["lookup_date"]
+        date_data_remote.__sp_insert_date = sp_list["insert_date"]
+        date_data_remote.__sp_insert_reference = sp_list["insert_reference"]
+        date_data_remote.__sp_insert_date_reference = sp_list["insert_date_reference"]
+        date_data_remote.__sp_insert_tweet_date = sp_list["insert_tweet_date"]
+        date_data_remote.__sp_truncate_tables = sp_list["truncate_table"]
+        date_data_remote.__sp_get_dates = sp_list["get_dates"]
         
         del sp_list, myy
-    
+        
     def truncate_tables(self):
+        # truncate tables
         self._db.call_sp(self.__sp_truncate_tables)      
+
+    """
+    def insert_date(self, id, date, description, from_ireland):        
+        # convert date into correct format
+        args = (int(id), date, description, int(from_ireland),)
+        self._db.call_sp(self.__sp_insert_date, args)
+    """
 
     def insert_date(self, date, description):        
         # convert date into correct format
@@ -59,6 +65,13 @@ class date_data(data):
         
         return date_id
     
+    """
+    def insert_reference(self, id, description, date, url, retrieved_date):
+        # List of arguments to send to stored procedure
+        args = (int(id), description, date, url, retrieved_date,)
+        self._db.call_sp(self.__sp_insert_reference, args)
+    """    
+        
     def insert_reference(self, ref_description, ref_date_txt, ref_available_from, ref_retrived_txt):
         # Convert date to proper format
         if ref_date_txt:
@@ -79,68 +92,24 @@ class date_data(data):
     
     def insert_date_reference(self, date_id, reference_id):
         # List of arguments to send to stored procedure
-        args = (date_id, reference_id)
-        self._db.call_sp(self.__sp_insert_date_reference, args)
-
-    def get_dates_for_migration(self):
-        self._db.call_sp(self.__sp_get_dates_for_migration)        
-        results = self._db.get_results_dataframe()        
-        return results
-
-    def get_date_reference_for_migration(self):
-        self._db.call_sp(self.__sp_get_date_reference_for_migration)
-        results = self._db.get_results_dataframe()        
-        return results      
-
-    def get_reference_for_migration(self):
-        self._db.call_sp(self.__sp_get_reference_for_migration)
-        results = self._db.get_results_dataframe()        
-        return results     
-         
-    def insert_migration(self, id):
-        self._db.call_sp(self.__sp_insert_migration, (id, ))
-
-    def lookup_date(self, date, description):      
-        # convert date into correct format
-        date_str = data._convert_date(date)
-
-        args = (str(date_str), description)
-        self._db.call_sp(self.__sp_lookup_date, args)
-        results = self._db.get_results_dataframe()        
-        return results        
-
-class date_data_remote(data):
-    def __init__(self):
-        super().__init__('tanniest_sentimentanalysis', False)
-
-        # Get name of stored procedure
-        myy = my_yaml.my_yaml_mydb()
-        sp_list = myy.get_stored_procedure_names_sentimentanalysis()
-        date_data_remote.__sp_insert_date = sp_list["insert_date"]
-        date_data_remote.__sp_insert_reference = sp_list["insert_reference"]
-        date_data_remote.__sp_insert_date_reference = sp_list["insert_date_reference"]
-        date_data_remote.__sp_insert_tweet_date = sp_list["insert_tweet_date"]
-        
-        del sp_list, myy
-    
-    def insert_date(self, id, date, description, from_ireland):        
-        # convert date into correct format
-        args = (int(id), date, description, int(from_ireland),)
-        self._db.call_sp(self.__sp_insert_date, args)
-    
-    def insert_reference(self, id, description, date, url, retrieved_date):
-        # List of arguments to send to stored procedure
-        args = (int(id), description, date, url, retrieved_date,)
-        self._db.call_sp(self.__sp_insert_reference, args)
-        
-    def insert_date_reference(self, date_id, reference_id):
-        # List of arguments to send to stored procedure
         args = (int(date_id), int(reference_id))
         self._db.call_sp(self.__sp_insert_date_reference, args)
 
     def insert_tweet_date(self):
         self._db.call_sp(self.__sp_insert_tweet_date)
 
-
+    def lookup_date(self, date, description):      
+        # convert date into correct format
+        date_str = data._convert_date(date)
+        args = (str(date_str), str(description))
+        self._db.call_sp(self.__sp_lookup_date, args)
+        results = self._db.get_results_dataframe()        
+        return results        
+    
+    def get_dates(self):
+        # convert date into correct format
+        self._db.call_sp(self.__sp_get_dates)
+        results = self._db.get_results_dataframe()        
+        return results
     
 
