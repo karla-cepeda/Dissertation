d893a17fdbcaca4b2aab74d8640d1144be68a5b1
karla-cepeda <d00242569@student.dkit.ie>
2021-09-10 15:08:12 +0100

    Remove dates migration to remote db

--- Code/layer_logic/migration_logic.py
+++ Code/layer_logic/migration_logic.py
@@ -11,61 +11,6 @@ be used for a dashboard to show off the results.
 """
 from layer_data_access import tweet_data, date_data
 
-class date_migrate(object):
-    def __init__(self):
-        """
-        Constructor.
-        
-        """
-        date_migrate.__dated_local = date_data.date_data() # Create access to local database
-        date_migrate.__dated_remote = date_data.date_data_remote() #  Create access to remote database
-        
-    def __del__(self):
-        # Delete access objects
-        del date_migrate.__dated_local
-        del date_migrate.__dated_remote
-    
-    @staticmethod
-    def start_process():
-        """
-        Starts the migration process of date data.
-
-        Returns
-        -------
-        None.
-
-        """
-        print("Start date migration to remote db.")
-        df_dates = date_migrate.__dated_local.get_dates_for_migration()
-        df_dates_references = date_migrate.__dated_local.get_date_reference_for_migration()
-        df_references = date_migrate.__dated_local.get_reference_for_migration()
-        
-        if not df_dates is None:
-            # Copy all data from dates that havent been copied to remote db
-            for i in range(len(df_dates)):            
-                date_migrate.__dated_remote.insert_date(**{c: df_dates.loc[i,c] for c in df_dates.columns})
-            
-            for i in range(len(df_dates_references)):
-                date_migrate.__dated_remote.insert_date_reference(**{c: df_dates_references.loc[i,c] for c in df_dates_references.columns})
-
-            for i in range(len(df_references)):
-                date_migrate.__dated_remote.insert_reference(**{c: df_references.loc[i,c] for c in df_references.columns})
-
-            # Insert tweets and dates relation
-            date_migrate.__dated_remote.insert_tweet_date()
-        
-            # Register ids inserted in database
-            ids = df_dates.id
-            for i in ids:
-                date_migrate.__dated_local.insert_migration(i)
-                print("date", i, "has been inserted into remote db")
-
-        else:
-            print("No dates to migrate.")
-
-        print("Migration process has been completed.")
-
-
 class tweet_migrate(object):
     def __init__(self):
         tweet_migrate.__tweetd_local = tweet_data.tweet_data()
